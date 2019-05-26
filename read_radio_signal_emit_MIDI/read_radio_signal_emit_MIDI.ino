4cf3d20b9999298d800329a8427c6a1c60e9bae9
Robert Zacharias <bobby.zacharias@gmail.com>
2019-05-26 18:02:09 -0400

    expanding to four signals

--- read_radio_signal_emit_MIDI/read_radio_signal_emit_MIDI.ino
+++ read_radio_signal_emit_MIDI/read_radio_signal_emit_MIDI.ino
@@ -30,9 +30,12 @@
 const int RADIO_CE_PIN = 7;
 const int RADIO_CSN_PIN = 8;
 
+// length of data array to receive via radio
+const int ARRAY_LENGTH = 4;
+
 // values used for auto-ranging input seen
-int highest[3] = {0, 0, 0};
-int lowest[3] = {1023, 1023, 1023};
+int highest[ARRAY_LENGTH] = {0, 0, 0, 0};
+int lowest[ARRAY_LENGTH] = {1023, 1023, 1023, 1023};
 
 RF24 radio(RADIO_CE_PIN, RADIO_CSN_PIN);
 const byte address[6] = "00001";
@@ -52,7 +55,7 @@ void loop() {
   if (radio.available()) {
 
     // load received values into a local array
-    int readVal[3];
+    int readVal[ARRAY_LENGTH];
     radio.read(&readVal, sizeof(readVal));
 
     // simple autorange for normalizing values
@@ -62,7 +65,7 @@ void loop() {
     }
 
     // scale observed output to 0–127 MIDI range
-    int outVal[3];
+    int outVal[ARRAY_LENGTH];
     for (int i = 0; i < sizeof(readVal) / sizeof(readVal[0]); i++) {
       outVal[i] = map(readVal[i], lowest[i], highest[i], 0, 127);
     }
@@ -71,6 +74,7 @@ void loop() {
     controlChange(0, 10, outVal[0]);
     controlChange(0, 11, outVal[1]);
     controlChange(0, 12, outVal[2]);
+    controlChange(0, 13, outVal[3]);
 
     // Serial debugging
     for (int i = 0; i < sizeof(readVal) / sizeof(readVal[0]); i++) {
@@ -82,7 +86,7 @@ void loop() {
 }
 
 
-  void controlChange(byte channel, byte control, byte value) {
-    midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-    MidiUSB.sendMIDI(event);
-  }
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
